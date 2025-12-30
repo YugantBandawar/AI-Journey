1c8def64d8090265f55a4d6177b052292e22f74b
Yugant Bandawar <yabandawar@gmail.com>
2025-12-30 14:09:03 +0530

    assinment-1

---
+++ Python/Program1.py
@@ -0,0 +1,28 @@
+# print("Hello world","with pyhton")
+print("Hello \n world","with pyhton") 
+
+var =True
+print(not var)  #relational Operator
+
+a=10
+b=5
+
+#TYPE CONVERSION:Python automatically converts 
+print(a/b) #2.0 converts to floaT
+
+print(10.0+a) #20.0 :Type:float
+
+#Type Casting-done by developers
+
+#User input
+a=input("enter the value of a: ")
+print(a)
+b=input("enter the value of b: ")
+print(a+b) #105
+#in input by default it is taking as String
+
+a=int(input("enter the value of a: "))
+print(a)
+b=int(input("enter the value of b: "))
+print((a+b)/2)
+
---
+++ Python/assingment1.py
@@ -0,0 +1,53 @@
+#Write a program that asks the user for their name and age,then prints a sentence
+# name=input("Enter the name:")
+# age=int(input("Enter the age:"))
+# print("Hello",name,", you are",age,"years old!")
+
+
+#Take two numbers as input from the user and print their sum,difference,product,and quotient
+# a=10
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+#Ask the user to enter two integer sand one float.Convert them all to floats and print their average
+
+# a=int(input("Enter rthe value of a:"))
+# b=int(input("Enter the value of b:"))
+# c=float(input("Enter the value of c:"))
+# print((a+b+c)/3)
+
+#The user enters a string containing a number(e.g.,).Convert it to"45" 
+# •an integer
+# •a float
+# •a string again
+# Print all three values with their types
+
+# a=input("enter thye value of a:")
+
+# print(int(a),type(int(a)))
+# print(float(a),type(float(a)))
+# print(a,type(a))
+
+# print(10+3*2**2)
+
+# Write a program to swap the value
+
+# a=2
+# b=3
+
+# c=a
+# a=b
+# b=c
+# print(a)
+# print(b)
+
+#AsktheuserforatemperatureinCelsius(stringinput).Convertitto,thencalculateandprinttemperatureinFahrenheit\
+
+a=input("Enter the number")
+
+FT=(int(a)*9/5)+32
+print(FT)
